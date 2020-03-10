38cc62e49748432bf61f659956e281537df08c71
riekure <s2darl0uvgia24@gmail.com>
2020-03-10 23:55:43 +0900

    ApproachCharacter and VRBrake

---
+++ Assets/Scripts/Break.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Break : MonoBehaviour {
+
+    public GameObject explosion;
+
+    void OnCollisionEnter(Collision collision) 
+    {
+        // オブジェクトを消す
+        Destroy(gameObject);
+
+        // 爆発の prefab
+        Instantiate(explosion, gameObject.transform.position, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour {
+
+    private Rigidbody rigidbody;
+    private float moveSpeed = 5;
+
+    void Start () {
+        rigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Update () {
+
+        Vector3 primaryTouchPad = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad);
+
+        // 上はz+、下はz-、右はx+、左はx-
+        Vector3 position = new Vector3(primaryTouchPad.x, 0, primaryTouchPad.y);
+
+        rigidbody.velocity = position * moveSpeed;
+    }
+}
