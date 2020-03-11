a0a1624925393fdf494f3cd392a861aad559b0f2
riekure <s2darl0uvgia24@gmail.com>
2020-03-11 16:51:13 +0900

    add VRShooting

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    [SerializeField] private GameObject explosionPrefab;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+
+        Instantiate(explosionPrefab, gameObject.transform.position, Quaternion.identity);
+    }
+}
